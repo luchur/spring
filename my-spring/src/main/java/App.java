4017bc6f687d8115df02527837c9420193494f46
luchuangchuang <luchuangchuang@chunyu.me>
2020-11-07 22:55:58 +0800

    init debug demo

--- my-spring/src/main/java/App.java
+++ my-spring/src/main/java/App.java
@@ -1,20 +1,20 @@
-package com.example.spring;
-
-
-import com.example.spring.dto.HelloWorld;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 /**
- * Hello world!
- *
+ * @author luchuang
+ * @date 2020-11-07
  */
 public class App {
-	public static void main( String[] args ){
-		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("application.xml"));
-		HelloWorld helloWorld = (HelloWorld) beanFactory.getBean("helloWorld");
-		System.out.println(helloWorld.getHello());
-	}
 
+	public static void main(String[] args) {
+
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("application.xml"));
+
+		HelloWorld helloWorld = (HelloWorld) beanFactory.getBean("helloWorld");
+		System.out.println(helloWorld.getTestStr());
+
+	}
 }
---
+++ my-spring/src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author luchuang
+ * @date 2020-11-07
+ */
+
+public class HelloWorld {
+	private String testStr = "hello world!";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+
+}
